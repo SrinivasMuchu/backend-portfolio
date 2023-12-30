5a372858f04ca9be922b0b07ee242d1cfc0f7831
SrinivasMuchu <107020456+SrinivasMuchu@users.noreply.github.com>
2023-12-30 17:37:40 +0530

    Update getInTouch.controller.js

--- GetInTouch/getInTouch.controller.js
+++ GetInTouch/getInTouch.controller.js
@@ -3,14 +3,13 @@ const { contactFormModel } = require('../models/contactForm');
 
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+   
     service: 'gmail',
     auth: {
         user: 'srinivasmuchu14@gmail.com',
         pass: 'gxtfqumjokyqljwy',
     },
+    timeout: 100000
 });
 
 async function createGetInTouch(req, res) {
